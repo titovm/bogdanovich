c1eef24fff67216e0c5fb44349b526b453ab6164
Mark Titov <titov.mark@gmail.com>
2018-05-31 22:31:27 +0300

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,6 @@
+// $(function(){
+//     $('.page').css({'height':($(document).height())+'px'});
+//     $(window).resize(function(){
+//         $('.page').css({'height':($(document).height())+'px'});
+//     });
+// });
