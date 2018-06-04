1894a8607ba2ab25b6c41a2d33951d14d04a088b
Mark Titov <titov.mark@gmail.com>
2018-06-04 11:53:50 +0300

    edits

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,3 +6,28 @@
 // });
 
 AOS.init();
+
+
+// Counter
+var options = {
+  useEasing: true,
+  useGrouping: true,
+  separator: ' ',
+  decimal: '.',
+};
+var count1 = new CountUp('count-500', 0, 500, 0, 1.5, options);
+if (!count1.error) {
+  count1.start();
+}
+var count2 = new CountUp('count-1500', 0, 1500, 0, 1.5, options);
+if (!count2.error) {
+  count2.start();
+}
+var count3 = new CountUp('count-90000', 0, 90000, 0, 1.5, options);
+if (!count3.error) {
+  count3.start();
+}
+var count4 = new CountUp('count-3m', 0, 3000000, 0, 1.5, options);
+if (!count4.error) {
+  count4.start();
+}
